085d7dc9d86d00340ecb57e25fb8c38e7413ec8c
Lea <leapaetzoldd@gmail.com>
2025-01-21 19:15:09 +0100

    added jump

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,15 +1,18 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float _speed;
-    [SerializeField] private Rigidbody _rb;
+    [SerializeField, Header("Settings")] private float _speed;
+    [SerializeField] private float _jumpForce;
+    [SerializeField, Header("References")] private Rigidbody _rb;
+    [SerializeField] private Transform _cameraTransform;
 
     private Vector2 _movementInput;
-    [SerializeField] private Transform _cameraTransform;
+    private RaycastHit _hit;
     private void Awake()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -22,17 +25,33 @@ public class PlayerController : MonoBehaviour
         Move();
     }
 
-    public void OnMOVE(InputAction.CallbackContext context)
+    public void OnMOVE(CallbackContext context)
     {
         _movementInput = context.ReadValue<Vector2>();
     }
 
+    public void OnJUMP(CallbackContext context)
+    {
+        if(context.performed && IsGrounded())
+        {
+            Jump();
+        }
+    }
+
     private void Move()
     {
-        //Vector3 movementVector = (transform.right * _movementInput.x + transform.forward * _movementInput.y).normalized;
-        //_rb.velocity = movementVector * _speed;
+        Vector3 movementVector = Quaternion.Euler(0, _cameraTransform.eulerAngles.y, 0) * new Vector3(_movementInput.x * _speed, 0, _movementInput.y * _speed);
+        _rb.velocity = new Vector3(movementVector.x, _rb.velocity.y, movementVector.z);
+    }
 
-        Vector3 movementVector = Quaternion.Euler(0, _cameraTransform.eulerAngles.y, 0) * new Vector3(_movementInput.x, 0, _movementInput.y);
-        _rb.velocity = movementVector * _speed;
+    private void Jump()
+    {
+        _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+    }
+
+    private bool IsGrounded()
+    {
+        if(Physics.Raycast(transform.position, Vector3.down, out _hit, 1)) return true;
+        else return false;
     }
 }
